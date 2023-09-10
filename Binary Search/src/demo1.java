253e1966317b4b8e5d4efa3ec1f18ecf18b18263
Mohd Ajlal <ajlal00786@gmail.com>
2023-09-11 00:02:53 +0530

    DSA

--- Binary Search/src/demo1.java
+++ Binary Search/src/demo1.java
@@ -1,20 +1,47 @@
-class Solution {
-    public int findGCD(int[] nums) {
-        int min= nums[0];
-        int max = nums[0];
-        for(int ele : nums){
-            if(ele < min){
-                min = ele;
+import java.util.Scanner;
+
+public class demo1 {
+    public static void smart(int x) {
+        int i = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        while (i <= x) {
+            int m, n;
+            m = scanner.nextInt();
+            n = scanner.nextInt();
+
+            if ((m >= 1 && m <= 1000000) && (n >= 1 && n <= 1000000)) {
+                int aayush = -1;
+                int harsh = 0;
+
+                while (true) {
+                    aayush = aayush + 2;
+                    harsh = harsh + 2;
+
+                    if (m < aayush) {
+                        System.out.println("Harshit");
+                        break;
+                    } else if (n < harsh) {
+                        System.out.println("Aayush");
+                        break;
+                    }
+                }
             }
-            if(ele>max){
-                max = ele;
-            }
+
+            i++;
         }
-        while(min!=0){
-            int temp = min;
-            min = max % min;
-            max = temp;
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        if (x >= 1 && x <= 1000) {
+            smart(x);
         }
-        return max;
+
+        scanner.close();
     }
 }
--- Binary Search/src/leetcode410.java
+++ Binary Search/src/leetcode410.java
@@ -1,2 +1,93 @@
 public class leetcode410 {
+
+    static int split_array(int nums[], int k){
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            start = Math.max(start, nums[i]); // in the end of the loop it will contain the maximum item from the array
+            end += nums[i];
+        }
+//        binary search
+        while(start<end){
+//        try for the middle as the potential answer
+            int mid = start + (end - start)/2;
+
+//            calculate how many pieces you can divide this in with this max sum
+
+            int sum = 0;
+            int pieces = 1;
+            for(int num: nums){
+                if(sum + num > mid){
+//                    you can't add this in subarray, make new one
+//                    say you add this num in new subarray, then num = sum
+                    sum = num;
+                    pieces++;
+                }else{
+                    sum += num;
+                }
+            }
+
+            if(pieces>k){
+                start = mid +1;
+            }else {
+                end = mid;
+            }
+        }
+        return end; // start == end
+    }
+    public static void main(String[] args) {
+        int[] array = {7,2,5,10,8};
+        int m = 2;
+        int out = split_array(array, m);
+        System.out.println(out);
+    }
 }
+
+
+/*
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        //range[max(nums), sum(nums)]
+        int lo = 0, hi = 0;
+        for(int num: nums){
+            if(num > lo){
+                lo = num;
+            }
+            hi += num;
+        }
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(mIsValid(nums, m, mid)){
+                hi = mid - 1;
+            }else{
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    private boolean mIsValid(int[] nums, int m, int val){
+        //if group into m groups, each group's sum <= val
+        int groupNeed = 0, curSum = 0;
+        for(int num: nums){
+            curSum += num;
+            if(curSum > val){
+                ++groupNeed;
+                curSum = num;
+            }
+        }
+        if(curSum > 0){
+            ++groupNeed;
+        }
+        return groupNeed <= m;
+    }
+}
+
+//Runtime
+//0ms
+//Beats 100.00%of users with Java
+//Memory
+//39.69MB
+//Beats 96.54%of users with Java
+ */
