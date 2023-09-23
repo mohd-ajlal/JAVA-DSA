4226168f647ec9cfeee3f5adf4f6db45c78eaab2
Mohd Ajlal <ajlal00786@gmail.com>
2023-09-23 14:39:11 +0530

    DSA

---
+++ pattern/src/Pattern.java
@@ -0,0 +1,13 @@
+public class Pattern {
+
+    static void pattern1(int n){
+        for(int row = 1; row<=n; row++){
+            for(int col = 1; col <=row; col++){
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
