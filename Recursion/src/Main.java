cfa875cde04886c4bbfbe14c4ba3207b30da3641
Mohd Ajlal <ajlal00786@gmail.com>
2023-11-17 03:18:06 +0530

    JAVA

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        message0();
     }
+
+    static void message0(){
+        System.out.println("Hello World");
+        message1();
+    }
+    static void message1(){
+        System.out.println("Hello World");
+        message2();
+    }
+    static void message2(){
+        System.out.println("Hello World");
+        message3();
+    }static void message3(){
+        System.out.println("Hello World");
+        message4();
+    }
+    static void message4(){
+        System.out.println("Hello World");
+    }
+
 }
