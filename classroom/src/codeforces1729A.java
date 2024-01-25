4b02b3696af51467fc63d67e34d30a98e7af9669
Mohd Ajlal <ajlal00786@gmail.com>
2024-01-25 23:11:36 +0530

    DSA

---
+++ classroom/src/codeforces1729A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class codeforces1729A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i<t; i++){
+            
+        }
+    }
+}
---
+++ classroom/src/codeforces_1335A.java
@@ -0,0 +1,11 @@
+//import java.util.Scanner;
+//
+//public class codeforces_1335A {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        for(int i = 0 i<t; i++){
+//
+//        }
+//    }
+//}
