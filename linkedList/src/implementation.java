5f56fa7b6fb6e17c42f5fae94c99f2a6a6f0375a
Mohd Ajlal <ajlal00786@gmail.com>
2024-02-13 01:19:49 +0530

    singly linked list

---
+++ linkedList/src/implementation.java
@@ -0,0 +1,77 @@
+public class implementation {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static class linkedList{
+        Node head = null;
+        Node tail = null;
+
+        void insertAtEnd(int val){
+            Node temp = new Node(val);
+            if(head == null){
+                head = temp;
+            }else{
+                tail.next = temp;
+            }
+            tail= temp;
+        }
+
+        void insertAtFirst(int val){
+            Node temp = new Node(val);
+            if(head==null){
+                tail = temp;
+            }else{
+                temp.next = head;
+            }
+            head = temp;
+        }
+
+        void insertAt(int val, int index){
+            Node t = new Node(val);
+            Node temp = head;
+            for(int i = 1; i<=index-1; i++){
+                temp = temp.next;
+            }
+            t.next = temp.next;
+            temp.next = t;
+        }
+
+        void display(){
+            Node temp =head;
+            while(temp!= null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+        int size(){
+            Node temp = head;
+            int count = 0;
+            while(temp!=null){
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+    }
+    public static void main(String[] args) {
+        linkedList ll = new linkedList();
+        ll.insertAtEnd(5);
+        ll.insertAtEnd(6);
+        ll.insertAtEnd(45);
+        ll.display();
+        System.out.println(ll.size());
+        System.out.println("First");
+        ll.insertAtFirst(50);
+        ll.display();
+        System.out.println("insert At Mid: ");
+        ll.insertAt(100, 2);
+        ll.display();
+    }
+}
